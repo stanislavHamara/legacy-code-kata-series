4190151167deb18ae0d4061b3a18419e66a3ad31
James Billings <james.billings@red-gate.com>
2016-04-04 09:45:21 +0100

    Add extra tests for Backstage Pass and Aged Brie cases

--- src/GildedRose.Tests/ItemUpdaterTests.cs
+++ src/GildedRose.Tests/ItemUpdaterTests.cs
@@ -60,5 +60,26 @@ namespace GildedRose.Tests
             Assert.AreEqual(4, item.SellIn);
         }
 
+        [Test]
+        public void BackstagePassItemShouldHaveZeroQualityWhenSellInBelowZero()
+        {
+            var item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 6 };
+
+            Program.UpdateItem(item);
+
+            Assert.AreEqual(0, item.Quality);
+            Assert.AreEqual(-1, item.SellIn);
+        }
+
+        [Test]
+        public void AgedBrieQualityIncreasesTwiceAsFastWhenSellInIsLessThanZero()
+        {
+            var item = new Item { Name = "Aged Brie", SellIn = 0, Quality = 6 };
+
+            Program.UpdateItem(item);
+
+            Assert.AreEqual(8, item.Quality);
+            Assert.AreEqual(-1, item.SellIn);
+        }
     }
 }
