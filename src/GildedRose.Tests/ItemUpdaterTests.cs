0f9ac2098497c17b48473cb413ada9e17d1a21b5
James Billings <james.billings@red-gate.com>
2016-04-04 09:39:19 +0100

    Add tests to cover Backstage Pass special cases

--- src/GildedRose.Tests/ItemUpdaterTests.cs
+++ src/GildedRose.Tests/ItemUpdaterTests.cs
@@ -37,5 +37,28 @@ namespace GildedRose.Tests
             Assert.AreEqual(4, item.Quality);
             Assert.AreEqual(-1, item.SellIn);
         }
+
+        [Test]
+        public void BackstagePassItemShouldIncreaseQualityTwiceAsFastWhenSellInLessThanElevenDays()
+        {
+            var item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 6 };
+
+            Program.UpdateItem(item);
+
+            Assert.AreEqual(8, item.Quality);
+            Assert.AreEqual(9, item.SellIn);
+        }
+
+        [Test]
+        public void BackstagePassItemShouldIncreaseQualityThreeTimesAsFastWhenSellInLessThanSixDays()
+        {
+            var item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 6 };
+
+            Program.UpdateItem(item);
+
+            Assert.AreEqual(9, item.Quality);
+            Assert.AreEqual(4, item.SellIn);
+        }
+
     }
 }
