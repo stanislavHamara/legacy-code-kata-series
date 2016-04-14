366c2189941e5fbae677a9bdc17e402c0b74ad84
Michael Clark <michael.clark@red-gate.com>
2016-04-14 14:44:12 +0100

    R# Extract Fields - More item names

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -9,6 +9,10 @@ namespace GildedRose.Console
         private const string BackstagePassName = "Backstage passes to a TAFKAL80ETC concert";
         private const string AgedBrieName = "Aged Brie";
         private const string SulfurasHandOfRagnarosName = "Sulfuras, Hand of Ragnaros";
+        private const string DexterityVestName = "+5 Dexterity Vest";
+        private const string ElixirOfTheMongooseName = "Elixir of the Mongoose";
+        private const string ConjuredManaCakeName = "Conjured Mana Cake";
+
         private IList<Item> Items;
 
         internal static void Main(string[] args)
@@ -26,9 +30,9 @@ namespace GildedRose.Console
             {
                 Items = new List<Item>
                 {
-                    new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                    new Item {Name = DexterityVestName, SellIn = 10, Quality = 20},
                     new Item {Name = AgedBrieName, SellIn = 2, Quality = 0},
-                    new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                    new Item {Name = ElixirOfTheMongooseName, SellIn = 5, Quality = 7},
                     new Item {Name = SulfurasHandOfRagnarosName, SellIn = 0, Quality = 80},
                     new Item
                     {
@@ -36,7 +40,7 @@ namespace GildedRose.Console
                         SellIn = 15,
                         Quality = 20
                     },
-                    new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                    new Item {Name = ConjuredManaCakeName, SellIn = 3, Quality = 6}
                 }
             };
 
