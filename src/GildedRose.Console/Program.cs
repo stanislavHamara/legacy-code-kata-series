e916112f6d1cfb17cc7d8cd06b4b4712ab847205
Michael Clark <michael.clark@red-gate.com>
2016-06-17 15:46:10 +0100

    Add item type to input data

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -23,12 +23,13 @@ namespace GildedRose.Console
             {
                 Items = new List<Item>
                 {
-                    new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                    new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                    new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                    new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                    new Item {ItemType = ItemType.Perishable, Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                    new Item {ItemType = ItemType.Ageing, Name = "Aged Brie", SellIn = 2, Quality = 0},
+                    new Item {ItemType = ItemType.Perishable, Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                    new Item {ItemType = ItemType.Legendary, Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
                     new Item
                     {
+                        ItemType = ItemType.DesirableEvent,
                         Name = "Backstage passes to a TAFKAL80ETC concert",
                         SellIn = 15,
                         Quality = 20
